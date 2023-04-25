642e2d01d6efe75f3c8156cc45fc5490a5b85f84
3talks <111962208+3talks@users.noreply.github.com>
2023-04-25 21:53:05 +0545

    Add files via upload

---
+++ script.js
@@ -0,0 +1,81 @@
+
+$(document).ready(function(){
+    $(".navbar-toggler").click(function(){
+        $(".navbar-toggler").toggleClass("change");
+    })
+})
+
+
+
+
+function payment() {
+    var x = document.getElementById("click");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function payment1() {
+    var x = document.getElementById("click1");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  function payment2() {
+    var x = document.getElementById("click2");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
+  
+  function payment4() {
+    var x = document.getElementById("clickk");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+ 
+
+  function pay(){
+    window.location = "payment.html";
+  }
+
+  
+  function payy(){
+    window.location = "pay.html";
+  }
+
+  function price(){
+    window.location = "video.html"
+  }
+
+  function price1(){
+    window.location = "price.html"
+  }
+
+  function log(){
+    window.location = "login1.html"
+  }
+
+  function port(){
+    window.location = "portal.html"
+
+  }
+
+  function sum(){
+    window.location = "price.html"
+  }
+
+  function pay2(){
+    window.location = "pay2.html";
+  }
